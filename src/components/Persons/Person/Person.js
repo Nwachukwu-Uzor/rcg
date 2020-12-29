577174e1bebafcd1e1cfa7bf2c2b6d6c04849bc3
uzor1997 <66738486+uzor1997@users.noreply.github.com>
2020-12-29 20:43:09 +0100

    adding ref to the project

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,12 +6,28 @@ import withClass from '../../hoc/withClass'
 
 
 class Person extends Component {
+    constructor(props) {
+        super(props)
+        this.inputElementRef = React.createRef()
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render () {
         console.log('[Person.js] rendering...')
         return (
             <Auxilliary>
                 <p onClick={this.props.clicked}>I am {this.props.name} and I am {this.props.age} years old!</p>
-                <input type='text' onChange={this.props.changed} value={this.props.name}/> 
+                <input 
+                    type='text' 
+                    onChange={this.props.changed} 
+                    value={this.props.name}
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
+                /> 
             </Auxilliary>
         )
     }
